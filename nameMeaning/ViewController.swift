715156f4327c3f9d7ec719c640a3516a709c673e
DanielleSmith0 <Danielahsmith@icloud.com>
2023-06-07 09:35:51 -0600

    Initial Commit

---
+++ nameMeaning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nameMeaning
+//
+//  Created by Danielle Smith on 6/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
